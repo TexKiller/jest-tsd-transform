699fce15c13b2d6f35acb41ccd709879c5c89c49
Faister Cabrera Carvalho <Tex.Killer@hotmail.com>
2023-03-09 15:09:22 -0300

    Embed ts-jest@27.1.3 and fix transform

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,21 @@ import { awaitAsserts } from "./utils/transform/await/asserts";
 import { awaitNamespaces } from "./utils/transform/await/namespaces";
 import { expectError } from "./utils/transform/expect-error";
 import { importAsync, importFrom } from "./utils/transform/import";
+import TsJest, { TransformOptionsTsJest } from "ts-jest";
+import { env } from "process";
+
+env.TS_JEST_DISABLE_VER_CHECKER = "1";
+
+const tsJest = TsJest.createTransformer();
 
 module.exports = {
   canInstrument: false,
   getCacheKey: jestCreateCacheKeyFunction(),
-  process: (src: string | { code: string }) => {
+  process: (
+    src: string | { code: string },
+    filePath: string,
+    options: TransformOptionsTsJest
+  ) => {
     src = typeof src === "string" ? src : src.code;
     src = expectError(src);
     src = awaitAsserts(src);
@@ -15,7 +25,8 @@ module.exports = {
     src = importFrom(src);
     src = importFrom(src);
     src = importAsync(src);
+    src = tsJest.process(src, filePath, options);
 
-    return src;
+    return { code: src };
   },
 };
